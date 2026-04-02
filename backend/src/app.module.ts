723ed0b8f6b680353356b5858dddcff4b1374710
Cicero <higormc2015@gmail.com>
2026-04-02 15:14:34 -0300

    feat: configuracao da conexao entre o TypeORM e o modulo de historico

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,11 @@ import { ExternalApiModule } from './external-api/external-api.module'
 import { IndicatorsModule } from './indicators/indicators.module'
 import { ScheduleModule } from '@nestjs/schedule'
 import { SchedulerModule } from './scheduler/scheduler.module'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { RedisModule } from './redis/redis.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { HistoryModule } from './history/history.module'
+import { IndicatorHistory } from './history/entities/indicator-history.entity'
 
 @Module({
   imports: [
@@ -12,10 +15,21 @@ import { RedisModule } from './redis/redis.module'
       isGlobal: true,
     }),
     ScheduleModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: [IndicatorHistory],
+        synchronize: true,
+      }),
+    }),
     ExternalApiModule,
     IndicatorsModule,
     SchedulerModule,
     RedisModule,
+    HistoryModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
