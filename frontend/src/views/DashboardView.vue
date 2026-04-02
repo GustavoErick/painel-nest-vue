4c2fa8c2cb583cc4a4da2a0a7fe85d3e588b4bf3
Gustavo Erick <gustavoerickv@gmail.com>
2026-04-02 13:28:20 -0300

    feat: adiciona painéis na home

---
+++ frontend/src/views/DashboardView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import RealtimeIndicatorsPanel from '@/components/RealtimeIndicatorsPanel.vue'
+</script>
+
+<template>
+  <section class="flex flex-col gap-4">
+    <header class="flex flex-col gap-2">
+      <h2 class="m-0 text-2xl font-semibold text-[var(--p-text-color)]">
+        Indicadores em tempo real
+      </h2>
+    </header>
+
+    <RealtimeIndicatorsPanel />
+  </section>
+
+  <section class="flex flex-col gap-4 mt-15">
+    <header class="flex flex-col gap-2">
+      <h2 class="m-0 text-2xl font-semibold text-[var(--p-text-color)]">Dados históricos</h2>
+    </header>
+
+    <p>Gráfico de dados histórcios aqui</p>
+  </section>
+</template>
