52fd36a895958c5cd685718eddf6efb9d2d7fd4a
Gustavo Erick <gustavoerickv@gmail.com>
2026-04-02 09:39:50 -0300

    chore: adiciona o setup inicial `primevue`

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,22 @@
-import './assets/main.css'
+/* eslint-disable vue/no-reserved-component-names */
+/* eslint-disable vue/multi-word-component-names */
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
 app.use(createPinia())
 app.use(router)
 
